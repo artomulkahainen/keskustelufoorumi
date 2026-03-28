97f3be18427dcc915ac6d14453d63241cce0050d
g2qdh6f9cy-glitch <g2qdh6f9cy@privaterelay.appleid.com>
2026-03-28 18:29:17 +0200

    post endpoint for new post created

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,20 @@
 import { Hono } from "hono";
+import { Post } from "./models/Post.model.ts";
 
 export const app = new Hono();
 
 app.get("/health", async (c) => {
   return c.json({ status: "ok" });
 });
+
+
+const POSTS_ENDPOINT = '/posts'
+app.post(POSTS_ENDPOINT, async (c) => {
+  const body = await c.req.json();
+  const user = body.user;
+  const content = body.content;
+
+  const newPost = await Post.create({createdBy: user, content});
+  return c.json(newPost);
+});
+
