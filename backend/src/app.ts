8c4691bb2bbc602634437594945a6b928bdbd84a
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2026-03-28 19:24:00 +0200

    Add prettier conf

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,23 @@
-import { Hono } from "hono";
-import { Post } from "./models/Post.model.ts";
+import { Hono } from 'hono';
+import { Post } from './models/Post.model.ts';
 
 export const app = new Hono();
 
 app.onError((err, c) => {
-  console.error(err);
-  return c.text("Internal Server Error", 500);
+    console.error(err);
+    return c.text('Internal Server Error', 500);
 });
 
-app.get("/health", async (c) => {
-  return c.json({ status: "ok" });
+app.get('/health', async (c) => {
+    return c.json({ status: 'ok' });
 });
 
-const POSTS_ENDPOINT = "/posts";
+const POSTS_ENDPOINT = '/posts';
 app.post(POSTS_ENDPOINT, async (c) => {
-  const body = await c.req.json();
-  const user = body.user;
-  const content = body.content;
+    const body = await c.req.json();
+    const user = body.user;
+    const content = body.content;
 
-  const newPost = await Post.create({ createdBy: user, content });
-  return c.json(newPost);
+    const newPost = await Post.create({ createdBy: user, content });
+    return c.json(newPost);
 });
